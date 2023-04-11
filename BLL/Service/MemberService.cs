bcd4697bd72647f97d48908166e0ac2fbf1e54e6
Azran Hossain <mr69751@gmail.com>
2023-04-11 23:20:21 +0600

    Refac: Change methods to expression body

--- BLL/Service/MemberService.cs
+++ BLL/Service/MemberService.cs
@@ -3,35 +3,22 @@ using DAL;
 using DAL.Models;
 using System.Collections.Generic;
 using AutoMapper;
+using DAL.Interface;
 using DAL.Repository;
 
 namespace BLL.Service
 {
     public static class MemberService
     {
-        public static bool CreateMember(MemberDto memberDto)
-        {
-            var memberRepository = DataFactory.MemberRepository();
-            var member = Mapper.Map(memberDto, new Members());
-            return memberRepository.Add(member);
-        }
+        private static readonly IRepository<Members, int, bool> MemberRepository = DataFactory.MemberRepository();
 
-        public static List<MemberDto> GetMembers()
-        {
-            var memberRepository = DataFactory.MemberRepository();
-            return Mapper.Map(memberRepository.GetAll(), new List<MemberDto>());
-        }
+        public static bool CreateMember(MemberDto memberDto) =>
+            MemberRepository.Add(Mapper.Map(memberDto, new Members()));
 
-        public static MemberDto GetMember(int id)
-        {
-            var memberRepository = DataFactory.MemberRepository();
-            return Mapper.Map(memberRepository.Get(id), new MemberDto());
-        }
+        public static List<MemberDto> GetMembers() => Mapper.Map(MemberRepository.GetAll(), new List<MemberDto>());
 
-        public static bool DeleteMember(int id)
-        {
-            var memberRepository = DataFactory.MemberRepository();
-            return memberRepository.Delete(id);
-        }
+        public static MemberDto GetMember(int id) => Mapper.Map(MemberRepository.Get(id), new MemberDto());
+
+        public static bool DeleteMember(int id) => MemberRepository.Delete(id);
     }
 }
